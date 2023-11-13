ec8649763bd81b2c357ea1ad92f0baf98b47a2f2
robiya <robiya365@icloud.com>
2023-11-13 22:25:10 +0500

    start

---
+++ les1/les1-hw1/script.js
@@ -0,0 +1,12 @@
+//Задание 1
+const celsius = 14
+const faranheit = (9/5) * celsius + 32
+
+console.log(celsius);
+console.log(faranheit);
+
+//Задание 2
+let firstName = 'Robiya'
+let admin = firstName
+
+console.log(admin);
